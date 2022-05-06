cdb3af7dcd6be82adc803f5fdde2f5b53e9aa551
Chirag Goyal <goyalchirag98@gmail.com>
2022-05-07 01:55:30 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (67.19%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=3)
+            return n;
+        int arr[] = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+        for(int i=4; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+    }
+}
+// 4
+// 1 1 1 1
+// 1 1 2
+// 1 2 1
+// 2 1 1
+// 2 2
+    
+    
+// 5
+// 1 1 1 1 1
+// 1 1 1 2
+// 1 1 2 1
+// 1 2 1 1
+// 2 1 1 1
+// 1 2 2
+// 2 1 2
+// 2 2 1
