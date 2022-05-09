5f272299a0e090a0c9ac14f91c8ed568f95b0569
Chirag Goyal <goyalchirag98@gmail.com>
2022-05-10 00:17:21 +0530

    Time: 1 ms (94.50%), Space: 41.6 MB (32.18%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,22 +1,35 @@
 class Solution {
     public boolean isHappy(int n) {
         
-        while(n>9)
-        {
-            int t=n;
-            int k=0;
-            while(t>0)
-            {
-                int rem=t%10;
-                k+=rem*rem;
-                t=t/10;
+        // while(n>9)
+        // {
+        //     int t=n;
+        //     int k=0;
+        //     while(t>0)
+        //     {
+        //         int rem=t%10;
+        //         k+=rem*rem;
+        //         t=t/10;
+        //     }
+        //     n=k;
+        //     System.out.println(k);
+        // }
+        // if(n == 1 ||n == 7)
+        //     return true;
+        // else
+        //     return false;
+        if(n<=0) return false;
+        while(true){
+            int sum=0;
+            while(n!=0){
+              sum+=(n%10)*(n%10);
+              n=n/10;
             }
-            n=k;
-            System.out.println(k);
+            if(sum/10==0){
+               if(sum==1||sum==7) return true;
+               else return false;
+            }
+            n=sum;
         }
-        if(n == 1 ||n == 7)
-            return true;
-        else
-            return false;
     }
 }
