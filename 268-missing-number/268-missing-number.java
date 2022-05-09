4abc8c8077a4ad59051507a4fe0cffeeea0a1323
Chirag Goyal <goyalchirag98@gmail.com>
2022-05-10 00:40:18 +0530

    Time: 0 ms (100.00%), Space: 51.7 MB (19.31%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,11 +1,8 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int size = nums.length;
-        int maxSum = (size*(size+1))/2;
-        
-        int arrSum=0;
-        for(int i=0; i<size; i++)
-            arrSum+=nums[i];
-        return maxSum-arrSum;
+        int maxSum = (nums.length*(nums.length+1))/2;
+        for(int val:nums)
+            maxSum-=val;
+        return maxSum;
     }
 }
