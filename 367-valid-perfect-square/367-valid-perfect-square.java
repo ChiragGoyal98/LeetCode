211c7874c3f0ff243897a65466e24ec08a3d4ffe
Chirag Goyal <goyalchirag98@gmail.com>
2022-05-11 01:06:10 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (84.15%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        // for(int i=0; i*i<=num; i++)
+        // {
+        //     if((i*i)==num)
+        //         return true;
+        // }
+        // return false;
+        if(num == 1)
+        return true;
+
+        long low = 1,
+            high = num / 2,
+            mid = 0;
+
+        long nums = (long)num;
+
+        while(low <= high)
+        {
+            mid = low + (high - low) / 2;
+
+            if((mid * mid) == nums)
+                return true;
+            else if( (mid * mid) < nums)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+
+
+        return false;
+    }
+}
