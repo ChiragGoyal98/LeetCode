66bafd84750fac9642b0ab9d64f802024386db91
Chirag Goyal <goyalchirag98@gmail.com>
2022-06-03 00:25:50 +0530

    Time: 1 ms (52.16%), Space: 49 MB (9.57%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.java
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -5,12 +5,8 @@ class Solution {
         int [][] ans = new int[m][n];
         
         for(int i=0; i<n; i++)
-        {
             for(int j=0; j<m; j++)
-            {
                 ans[j][i] = matrix[i][j];
-            }
-        }
         return ans;
     }
 }
