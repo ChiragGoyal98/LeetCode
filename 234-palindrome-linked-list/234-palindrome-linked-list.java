b4d891717f9000fb080b7835f4bbbb025b029fa6
Chirag Goyal <goyalchirag98@gmail.com>
2022-06-05 16:10:09 +0530

    Time: 4 ms (96.55%), Space: 58.6 MB (84.70%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,26 +9,78 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stck = new Stack<Integer>();
-        ListNode temp = head;
-        while(temp!=null)
+    
+    public ListNode midNode(ListNode node)
+    {
+        if(node==null || node.next == null)
+            return node;
+        
+        ListNode slow = node, fast = node;
+        while(fast.next!=null && fast.next.next!=null)
         {
-            stck.push(temp.val);
-            temp = temp.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
+        return slow;
+    }
+    
+    public ListNode rev(ListNode head){
+        if(head == null)
+            return head;
         
-        temp = head;
-        
-        while(temp!=null)
+        ListNode prev = null, curr = head;
+        while(curr!=null)
         {
-            
-            if(stck.peek() == temp.val)
-                stck.pop();
-            else
+            ListNode save = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = save;
+        }
+        return prev;
+    }
+    
+    public boolean isPalindrome(ListNode head) {
+//         Stack<Integer> stck = new Stack<Integer>();
+//         ListNode temp = head;
+//         while(temp!=null)
+//         {
+//             stck.push(temp.val);
+//             temp = temp.next;
+//         }
+        
+//         temp = head;
+        
+//         while(temp!=null)
+//         {
+//             if(stck.peek() == temp.val)
+//                 stck.pop();
+//             else
+//                 return false;
+//             temp = temp.next;
+//         }
+//         return true;
+        if(head == null)
+            return true;
+        
+        ListNode mid  = midNode(head);
+        
+        ListNode h2 = mid.next;
+        h2 = rev(h2);
+        mid.next = null;
+        
+        ListNode i = head, j = h2;
+        
+        while(i!=null && j!=null)
+        {
+            if(i.val != j.val)
                 return false;
-            temp = temp.next;
+            i = i.next;
+            j = j.next;
         }
         return true;
+        
+        
+        
+        
     }
 }
