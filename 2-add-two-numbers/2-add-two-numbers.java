a23ffa73b8a39e18c0a335a788110abbe8256f7a
Chirag Goyal <goyalchirag98@gmail.com>
2022-06-05 22:32:16 +0530

    Time: 2 ms (96.08%), Space: 42.4 MB (82.76%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,18 +10,47 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ln1 = l1, ln2 = l2, head = null, node = null;
-        int carry = 0, remainder = 0, sum = 0;
-        head = node = new ListNode(0);
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
         
-        while(ln1 != null || ln2 != null || carry != 0) {
-            sum = (ln1 != null ? ln1.val : 0) + (ln2 != null ? ln2.val : 0) + carry;
-            carry = sum / 10;
-            remainder = sum % 10;
-            node = node.next = new ListNode(remainder);
-            ln1 = (ln1 != null ? ln1.next : null);
-            ln2 = (ln2 != null ? ln2.next : null);
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
+        
+        int carry = 0;
+        
+        while(l1!= null || l2!=null || carry!= 0 )
+        {
+            int v1 = (l1!=null)? l1.val: 0;
+            int v2 = (l2!=null)? l2.val: 0;
+            
+            int val = v1 + v2 + carry;
+            int rem = val%10;
+            carry = val/10;
+            ListNode node = new ListNode(rem);
+            curr.next = node;
+            curr = curr.next;
+            if(l1!=null)
+                l1 = l1.next;
+            if(l2!=null)
+                l2 = l2.next;
+                
         }
         return head.next;
     }
 }
+
+//         ListNode ln1 = l1, ln2 = l2, head = null, node = null;
+//         int carry = 0, remainder = 0, sum = 0;
+//         head = node = new ListNode(0);
+        
+//         while(ln1 != null || ln2 != null || carry != 0) {
+//             sum = (ln1 != null ? ln1.val : 0) + (ln2 != null ? ln2.val : 0) + carry;
+//             carry = sum / 10;
+//             remainder = sum % 10;
+//             node = node.next = new ListNode(remainder);
+//             ln1 = (ln1 != null ? ln1.next : null);
+//             ln2 = (ln2 != null ? ln2.next : null);
+//         }
+//         return head.next;
