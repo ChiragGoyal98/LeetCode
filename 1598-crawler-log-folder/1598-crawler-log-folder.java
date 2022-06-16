4c26013dbf6be0c4995f8dde93ec5893331fb6c4
Chirag Goyal <goyalchirag98@gmail.com>
2022-06-16 22:16:30 +0530

    Time: 3 ms (19.75%), Space: 42.5 MB (59.11%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,38 +1,50 @@
 class Solution {
     public int minOperations(String[] logs) {
-        var stck = new Stack<>();
+//         var stck = new Stack<>();
         
-        for(String log : logs)
-        {
-            // switch (var){
-            //     case "../":{
-            //         if(stck.size()!=0)
-            //             stck.pop();
-            //         break;
-            //     }
-            //     case "./":{
-            //         break;
-            //     }
-            //     default:{
-            //         stck.push(true);
-            //         break;
-            //     }
-            // }
+//         for(String log : logs)
+//         {
+//             // switch (var){
+//             //     case "../":{
+//             //         if(stck.size()!=0)
+//             //             stck.pop();
+//             //         break;
+//             //     }
+//             //     case "./":{
+//             //         break;
+//             //     }
+//             //     default:{
+//             //         stck.push(true);
+//             //         break;
+//             //     }
+//             // }
+//             if(log.equals("../")){
+//                 if(!stck.empty())
+//                     stck.pop();
+//             }else if(log.equals("./")){
+
+//             }else{
+//                 stck.push(log);
+//             }
+//         }
+        
+//         // while(stck!=0)
+//         // {
+//         //     stck.pop();
+//         //     size++;
+//         // }
+//         return stck.size();
+        var stack = new Stack<String>();
+        for(var log : logs){
             if(log.equals("../")){
-                if(!stck.empty())
-                    stck.pop();
+                if(!stack.empty())
+                    stack.pop();
             }else if(log.equals("./")){
 
             }else{
-                stck.push(log);
+                stack.push(log);
             }
         }
-        
-        // while(stck!=0)
-        // {
-        //     stck.pop();
-        //     size++;
-        // }
-        return stck.size();
+        return stack.size();
     }
 }
