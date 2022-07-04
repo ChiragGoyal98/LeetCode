be01f23862483ff6fb28e24f0bfca767f0dce0e3
Chirag Goyal <goyalchirag98@gmail.com>
2022-07-04 15:10:47 +0530

    Time: 4 ms (70.03%), Space: 51.8 MB (44.36%) - LeetHub

---
+++ 135-candy/135-candy.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int candy(int[] ratings) {
+//         int lastCC = 1;
+//         int ans = 0;
+//         if(ratings[0] > ratings[1])
+//             ans = 2;
+//         else ans = 1;
+//         lastCC = ans;
+//         for(int i=1; i<ratings.length; i++)
+//         {
+//             if(ratings[i]>ratings[i-1])
+//             {
+//                 ans += lastCC + 1;
+//                 lastCC++;
+//             }
+//             else if(ratings[i] == ratings[i-1] && i!=ratings.length-1 && ratings[i] >ratings[i+1]){
+//                 ans+=lastCC+1;
+//                 lastCC++;
+//             }
+//             else{
+//                 ans++;
+//                 lastCC=1;
+//             }
+                          
+//         }
+//         return ans;
+        int []candies = new int[ratings.length];
+        int ans = 0;
+        Arrays.fill(candies,1);
+        
+        //l to r
+        for(int i=1; i<ratings.length; i++)
+        {
+            if(ratings[i]>ratings[i-1])
+                candies[i] = candies[i-1]+1;
+        }
+        //r to l
+        for(int i = ratings.length-2; i>=0; i--)
+        {
+            if(ratings[i]>ratings[i+1])
+                candies[i] = Math.max(candies[i+1]+1, candies[i]);
+        }
+        for(int ele: candies)
+            ans+=ele;
+        return ans;
+    }
+}
