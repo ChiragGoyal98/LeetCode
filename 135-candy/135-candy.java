f06bb36330f6518ed6b1a7cf808d5ff3733e96b6
Chirag Goyal <goyalchirag98@gmail.com>
2022-07-04 15:18:04 +0530

    Time: 4 ms (70.03%), Space: 51.4 MB (66.42%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,48 +1,20 @@
 class Solution {
     public int candy(int[] ratings) {
-//         int lastCC = 1;
-//         int ans = 0;
-//         if(ratings[0] > ratings[1])
-//             ans = 2;
-//         else ans = 1;
-//         lastCC = ans;
-//         for(int i=1; i<ratings.length; i++)
-//         {
-//             if(ratings[i]>ratings[i-1])
-//             {
-//                 ans += lastCC + 1;
-//                 lastCC++;
-//             }
-//             else if(ratings[i] == ratings[i-1] && i!=ratings.length-1 && ratings[i] >ratings[i+1]){
-//                 ans+=lastCC+1;
-//                 lastCC++;
-//             }
-//             else{
-//                 ans++;
-//                 lastCC=1;
-//             }
-                          
-//         }
-//         return ans;
-        int []candies = new int[ratings.length];
+        int size = ratings.length;
+        int []candiesDistributed = new int[ratings.length];
         int ans = 0;
-        Arrays.fill(candies,1);
-        
-        //l to r
-        for(int i=1; i<ratings.length; i++)
+        Arrays.fill(candiesDistributed,1);
+        for(int i=1; i<size; i++)
         {
             if(ratings[i]>ratings[i-1])
-                candies[i] = candies[i-1]+1;
+                candiesDistributed[i] = candiesDistributed[i-1]+1;
         }
-        //r to l
-        for(int i = ratings.length-2; i>=0; i--)
+        for(int i = size-2; i>=0; i--)
         {
             if(ratings[i]>ratings[i+1])
-                candies[i] = Math.max(candies[i+1]+1, candies[i]);
-            ans+=candies[i];
+                candiesDistributed[i] = Math.max(candiesDistributed[i+1]+1, candiesDistributed[i]);
+            ans+=candiesDistributed[i];
         }
-        // for(int ele: candies)
-        //     ans+=ele;
-        return ans + candies[ratings.length-1];
+        return ans + candiesDistributed[size-1];
     }
 }
