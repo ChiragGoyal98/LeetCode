6b7525dc110393b42acfe0d2b1ca4e0e0f20be30
Chirag Goyal <goyalchirag98@gmail.com>
2022-07-12 22:20:19 +0530

    Time: 80 ms (12.75%), Space: 48.8 MB (38.89%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isMatch(String str, String pattern) {
+        boolean [][]dp = new boolean[str.length()+1][pattern.length()+1];
+		for(int j=0; j<dp[0].length; j++)
+		{
+		    if(j==0)
+		        dp[0][j] = true;
+		    else if(pattern.charAt(j-1) == '*')
+		        dp[0][j] = dp[0][j-1];
+		}
+		for(int i=1; i<dp.length; i++)
+		{
+			for(int j=1; j<dp[0].length; j++)
+			{
+				
+				
+				if(pattern.charAt(j-1)=='?')
+					dp[i][j] = dp[i-1][j-1];
+				else if(pattern.charAt(j-1)=='*')
+					dp[i][j] = dp[i-1][j] || dp[i][j-1];
+				else if(str.charAt(i-1)==pattern.charAt(j-1))
+					dp[i][j] = dp[i-1][j-1];
+			}
+		}
+		return dp[str.length()][pattern.length()];
+    }
+}
