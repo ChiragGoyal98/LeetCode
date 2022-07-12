0b26b8b50ca90a15f048af88481897d53f1d9504
Chirag Goyal <goyalchirag98@gmail.com>
2022-07-12 23:23:57 +0530

    Time: 2 ms (97.01%), Space: 40.7 MB (97.12%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -9,43 +9,25 @@ class Solution {
 				dp[0][j] = dp[0][j-2];
 			}
 		}
+        
         for(int i=1; i<dp.length; i++)
-		{
-			for(int j=1; j<dp[0].length; j++)
-			{
-				char chp = p.charAt(j-1), chs = s.charAt(i-1);
-				if(chp == '.')
-					dp[i][j] = dp[i-1][j-1];
-				else if(chp =='*')
-				{
-					dp[i][j] = dp[i][j-2];
-					if(!dp[i][j] && (chs == p.charAt(j-2) || p.charAt(j-2) == '.'))
-						dp[i][j] = dp[i-1][j];
-				}
-				else if(chp == chs)
-					dp[i][j] = dp[i-1][j-1];
-			}
-		}
-		
-		return dp[s.length()][p.length()];
-//         for(int i=1; i<dp.length; i++)
-//         {
-//             for(int j=1; j<dp[0].length; j++)
-//             {
-//                 char chp = p.charAt(j-1), chs = s.charAt(i-1);
+        {
+            for(int j=1; j<dp[0].length; j++)
+            {
+                char chp = p.charAt(j-1), chs = s.charAt(i-1);
                 
-//                 if(chp == '.')
-//                     dp[i][j] = dp[i-1][j-1];
-//                 else if(chp == '*')
-//                 {
-//                     dp[i][j] = dp[i][j-2];
-//                     if(!dp[i][j] && ( chs == p.charAt(j-2) || p.charAt(j-2) == '.'))
-//                         dp[i][j] = true;
-//                 }
-//                 else if(chp == chs)
-//                     dp[i][j] = dp[i-1][j-1];
-//             }
-//         }
-//         return dp[s.length()][p.length()];
+                if(chp == '.')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(chp == '*')
+                {
+                    dp[i][j] = dp[i][j-2];
+                    if(!dp[i][j] && ( chs == p.charAt(j-2) || p.charAt(j-2) == '.'))
+                        dp[i][j] = dp[i-1][j];
+                }
+                else if(chp == chs)
+                    dp[i][j] = dp[i-1][j-1];
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 }
