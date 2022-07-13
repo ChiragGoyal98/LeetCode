61afaca0d5d3860ba75d90d8f3618ca6347e917e
Chirag Goyal <goyalchirag98@gmail.com>
2022-07-13 22:48:44 +0530

    Time: 1 ms (27.26%), Space: 40.9 MB (50.60%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        boolean []dp =new boolean[n+1];
+        for(int i=1; i<=n; i++)
+        {
+            if(i==1)
+                dp[i] = false;
+            else if(i==2)
+                dp[i] = true;
+            else{
+                for(int j=1; j*j <=i; j++){
+                    if(i%j == 0)
+                    {
+                        dp[i] = !dp[i-j];
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+}
