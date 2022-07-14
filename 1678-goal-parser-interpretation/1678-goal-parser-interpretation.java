7aea33db17c1b394ecee1c1e47b89389e68bc189
Chirag Goyal <goyalchirag98@gmail.com>
2022-07-15 01:57:34 +0530

    Time: 1 ms (90.11%), Space: 42.2 MB (48.55%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i<command.length(); i++)
+        {
+            if(command.charAt(i) == 'G')
+            {
+                ans.append("G");
+               // i++;
+            }
+            else if(command.charAt(i) == '(' && command.charAt(i+1) == ')')
+            {
+                ans.append("o");
+                //i+=2;
+            }
+            else if(command.charAt(i) == '(' && command.charAt(i+1) == 'a'){
+                ans.append("al");
+                //i+=4;
+            }
+        
+                //i++;
+        }
+        return ans.toString();
+    }
+}
