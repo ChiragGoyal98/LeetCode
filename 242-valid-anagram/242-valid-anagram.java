b84a52c5d910f7b5a239b26156d279534448e5e6
Chirag Goyal <goyalchirag98@gmail.com>
2022-07-28 18:42:00 +0530

    Time: 4 ms (85.70%), Space: 43.9 MB (51.50%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,7 +2,25 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())
             return false;
-        char ch1[] = s.toCharArray();
+        int freq[] = new int[26];
+        int sum = 0;
+        for(char c : s.toCharArray())
+        {
+            freq[c-'a'] ++;
+        }
+        for(char c : t.toCharArray())
+        {
+            freq[c-'a']--;
+        }
+        for(int i=0; i<26; i++)
+            if(freq[i]!=0)
+                return false;
+        return true;
+    }
+}
+
+/*
+char ch1[] = s.toCharArray();
         char ch2[] = t.toCharArray();
         
         Arrays.sort(ch1);
@@ -12,5 +30,4 @@ class Solution {
             if(ch1[i]!=ch2[i])
                 return false;
         return true;
-    }
-}
+        */
