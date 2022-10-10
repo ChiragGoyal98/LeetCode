87ecea1cfda0e0e76fc4030d2e56f72029e0fd11
Chirag Goyal <goyalchirag98@gmail.com>
2022-10-11 00:56:49 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (50.43%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -1,50 +1,63 @@
 class Solution {
     public String breakPalindrome(String palindrome) {
-        if(palindrome.length() == 1)
-            return "";
-        int len = palindrome.length();
-        char ans[] = new char[len];
-        if(allAs(palindrome))
-        {
-            for(int i=0; i<len-1; i++)
-                ans[i] = palindrome.charAt(i);
-            ans[len-1]='b';
-            return String.valueOf(ans);
-        }
-        boolean flag = false;
-        int i;
-        for(i=0; i<len; i++)
-        {
-            if(palindrome.charAt(i) != 'a' && flag == false)
-            {
-                ans[i] = 'a';
-                System.out.print(i);
-                flag = true;
-            }
-            else
-                ans[i] = palindrome.charAt(i);
-        }
-        String fAns = String.valueOf(ans);
-        if(allAs(fAns))
-        {
-            for(i=0; i<len-1; i++)
-                ans[i] = palindrome.charAt(i);
-            ans[len-1]='b';
-            return String.valueOf(ans);
-        }
-        // while(i<len)
+        // if(palindrome.length() == 1)
+        //     return "";
+        // int len = palindrome.length();
+        // char ans[] = new char[len];
+        // if(allAs(palindrome))
         // {
-        //     ans[i] = palindrome.charAt(i);
-        //     i++;
+        //     for(int i=0; i<len-1; i++)
+        //         ans[i] = palindrome.charAt(i);
+        //     ans[len-1]='b';
+        //     return String.valueOf(ans);
         // }
-        return String.valueOf(ans);
+        // boolean flag = false;
+        // int i;
+        // for(i=0; i<len; i++)
+        // {
+        //     if(palindrome.charAt(i) != 'a' && flag == false)
+        //     {
+        //         ans[i] = 'a';
+        //         System.out.print(i);
+        //         flag = true;
+        //     }
+        //     else
+        //         ans[i] = palindrome.charAt(i);
+        // }
+        // String fAns = String.valueOf(ans);
+        // if(allAs(fAns))
+        // {
+        //     for(i=0; i<len-1; i++)
+        //         ans[i] = palindrome.charAt(i);
+        //     ans[len-1]='b';
+        //     return String.valueOf(ans);
+        // }
+        // // while(i<len)
+        // // {
+        // //     ans[i] = palindrome.charAt(i);
+        // //     i++;
+        // // }
+        // return String.valueOf(ans);
+        if(palindrome.length() == 0 || palindrome.length() == 1){
+            return "";
+        }
+        char[] ch = palindrome.toCharArray();
+        for(int i = 0;i<ch.length/2;i++){
+            if(ch[i]-'a' != 0){
+                ch[i] = 'a';
+                return new String(ch);
+            }
+            
+        }
+        ch[ch.length-1] = 'b';
+        return new String(ch);
         
     }
-    public boolean allAs(String str)
-    {
-        for(int i=0; i<str.length(); i++)
-            if(str.charAt(i) !='a')
-                return false;
-        return true;
-    }
+    // public boolean allAs(String str)
+    // {
+    //     for(int i=0; i<str.length(); i++)
+    //         if(str.charAt(i) !='a')
+    //             return false;
+    //     return true;
+    // }
 }
