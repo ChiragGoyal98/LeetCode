9507cad8b8af39ba9d0956a1484d0c87ae101662
Chirag Goyal <goyalchirag98@gmail.com>
2022-11-01 01:30:41 +0530

    Time: 2 ms (42.28%), Space: 46.1 MB (21.74%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int r = matrix.length;
+        /*int r = matrix.length;
         int c = matrix[0].length;
         if(check(matrix, 0, 0, r, c) == false)
             return false;
@@ -17,9 +17,14 @@ class Solution {
                 return false;
         }
         return true;
-        
+      */  
+        for(int i=0; i<matrix.length; i++)
+            for(int j=0; j<matrix[0].length; j++)
+                if(i>0 && j>0 && matrix[i-1][j-1] != matrix[i][j])
+                    return false;
+        return true;
     }
-    public boolean check(int [][] matrix, int i, int j, int r, int c)
+    /*public boolean check(int [][] matrix, int i, int j, int r, int c)
     {
         int val = matrix[i][j];
         for(int m=i+1, n=j+1; m<r && n<c; m++, n++)
@@ -28,5 +33,5 @@ class Solution {
                 return false;
         }
         return true;
-    }
+    }*/
 }
